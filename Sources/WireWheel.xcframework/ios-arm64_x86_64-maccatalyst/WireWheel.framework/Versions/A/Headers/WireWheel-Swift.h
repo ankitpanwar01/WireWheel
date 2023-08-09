afb344d84b3a92e3c6217abdabf063496f9f26c4
Ankit Panwar <ankit.panwarmca@gmail.com>
2023-08-09 17:21:58 +0530

    version 0.0.8

--- Sources/WireWheel.xcframework/ios-arm64_x86_64-maccatalyst/WireWheel.framework/Versions/A/Headers/WireWheel-Swift.h
+++ Sources/WireWheel.xcframework/ios-arm64_x86_64-maccatalyst/WireWheel.framework/Versions/A/Headers/WireWheel-Swift.h
@@ -281,17 +281,9 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class NSCoder;
 @class NSString;
 @class NSBundle;
-@class NSCoder;
-
-SWIFT_CLASS("_TtC9WireWheel23ConnectorViewController")
-@interface ConnectorViewController : UIViewController
-- (void)viewDidLoad;
-- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
-@end
-
 
 SWIFT_CLASS("_TtC9WireWheel23DashboardViewController")
 @interface DashboardViewController : UIViewController
@@ -592,17 +584,9 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class NSCoder;
 @class NSString;
 @class NSBundle;
-@class NSCoder;
-
-SWIFT_CLASS("_TtC9WireWheel23ConnectorViewController")
-@interface ConnectorViewController : UIViewController
-- (void)viewDidLoad;
-- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
-@end
-
 
 SWIFT_CLASS("_TtC9WireWheel23DashboardViewController")
 @interface DashboardViewController : UIViewController
